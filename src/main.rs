0d806a385996edc4acc09473d1b2563c1070a8a3
Ken 🏈 <me163@mac.com>
2024-07-02 09:40:24 -0700

    first pass

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use chrono::Utc;
+use rustbucks::{mine::mine_pending_transactions, model::{blockchain::Blockchain, transaction::Transaction}};
+
+
+fn main() {
+    let bc = Blockchain::new();
+
+    let transaction = Transaction {
+        amount: 50,
+        sender: "me".to_string(),
+        receiver: "you".to_string(),
+        timestamp: Utc::now().timestamp(),
+    };
+
+    let new_block = mine_pending_transactions(&bc, vec![transaction]);
+    dbg!(new_block);
+}
---
+++ tests/one_node.rs
@@ -0,0 +1,34 @@
+use rustbucks::{mine::mine_pending_transactions, model::{node::Node, transaction::Transaction}};
+
+#[tokio::test]
+pub async fn one_node_should_accept_one_block() {
+    let mut node = Node::new();
+
+    let new_transactions = vec![
+        Transaction {
+            timestamp: 0,
+            sender: "Timmy".to_string(),
+            receiver: "Bobby".to_string(),
+            amount: 100,
+        },
+        Transaction {
+            timestamp: 1,
+            sender: "Alice".to_string(),
+            receiver: "Charlie".to_string(),
+            amount: 100,
+        },
+        Transaction {
+            timestamp: 2,
+            sender: "Jill".to_string(),
+            receiver: "Jane".to_string(),
+            amount: 20,
+        },
+    ];
+
+    // no need to submit to the node pending transactions for this #[cfg(test)]
+    let new_block = mine_pending_transactions(&node.blockchain, new_transactions);
+
+    let res = node.submit_mined_block(new_block).await;
+
+    assert_eq!(Ok(()), res);
+}
